91206ea0e5cd35e53bb3c52c03b1f62ec916f404
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-19 09:47:29 +0300

    fix on the task one

--- utility.c
+++ utility.c
@@ -79,10 +79,6 @@ void free_list(stack_t *head)
 {
 	stack_t *tmp;
 
-	if (head != NULL)
-		while (head->prev != NULL)
-			head = head->prev;
-
 	while ((tmp = head) != NULL)
 	{
 		head = head->next;
